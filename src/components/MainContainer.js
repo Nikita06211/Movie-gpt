b8b6562d6703b6766bf794d44205d7f6af8fa3c5
Nikita <bansalnikita06@gmail.com>
2024-11-29 23:22:32 +0530

    updates

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,12 +5,12 @@ import VideoBackground from './VideoBackground'
 
 const MainContainer = () => {
 
-    const movies = useSelector(store=>store.movies?.nowPlayingMovies)
+    const movies = useSelector(store=>store?.movies?.nowPlayingMovies)
 
     if(movies===null) return;
 
     const mainMovie = movies[0];
-    console.log(mainMovie);
+    console.log("mainMovie = "+mainMovie);
 
     const {original_title, overview, id} = mainMovie;
   return (
