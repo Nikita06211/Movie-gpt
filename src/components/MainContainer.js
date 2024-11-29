8a9e527c94b9f665ae3986e3395a045a645b40b9
Nikita <bansalnikita06@gmail.com>
2024-11-29 23:25:16 +0530

    debug

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import VideoTitle from './VideoTitle'
-import VideoBackground from './VideoBackground'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import VideoTitle from './VideoTitle';
+import VideoBackground from './VideoBackground';
 
 const MainContainer = () => {
+  const movies = useSelector((store) => store?.movies?.nowPlayingMovies);
 
-    const movies = useSelector(store=>store?.movies?.nowPlayingMovies)
+  // Check if movies is undefined or empty
+  if (!movies || movies.length === 0) {
+    console.log("No movies available in nowPlayingMovies.");
+    return <div className="text-white">Loading...</div>; // Optional fallback UI
+  }
 
-    if(movies===null) return;
+  const mainMovie = movies[0];
+  console.log("mainMovie =", mainMovie);
 
-    const mainMovie = movies[0];
-    console.log("mainMovie = "+mainMovie);
+  const { original_title, overview, id } = mainMovie || {}; // Fallback destructuring
 
-    const {original_title, overview, id} = mainMovie;
   return (
-    <div className='pt-[50%] bg-black md:pt-0'>
-      <VideoTitle title = {original_title} overview = {overview}/>
-      <VideoBackground movieId = {id}/>
+    <div className="pt-[50%] bg-black md:pt-0">
+      <VideoTitle title={original_title} overview={overview} />
+      <VideoBackground movieId={id} />
     </div>
-  )
-}
+  );
+};
 
-export default MainContainer
+export default MainContainer;
