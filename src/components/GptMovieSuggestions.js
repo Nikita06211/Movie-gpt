bb539e6b4c613c33372e64ddf06c586026c818bb
Nikita <bansalnikita06@gmail.com>
2024-11-29 22:55:23 +0530

    .

--- src/components/GptMovieSuggestions.js
+++ src/components/GptMovieSuggestions.js
@@ -11,7 +11,6 @@ const GptMovieSuggestions = () => {
     <div className="p-4 m-4 text-white bg-black bg-opacity-80">
       <div>
         {movieNames.map((movieName, index)=><MovieList key={movieName} title={movieName} movies = {movieResults[index]}/>)}
-        
       </div>
     </div>
   )
